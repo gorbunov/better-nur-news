6f708fb5caea0fa9524d0038fe4885331701c5a0
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2017-11-01 22:32:03 +0600

    First version

---
+++ src/js/convert.js
@@ -0,0 +1,6 @@
+    const news = document.querySelectorAll('.news__caption, .article__caption');
+    for (let i = 0; i < news.length; i++) {
+        let headline = news[i];
+        headline.textContent = 'Вопреки здравому смыслу, ' + headline.textContent;
+        console.log(headline.textContent);
+    }
