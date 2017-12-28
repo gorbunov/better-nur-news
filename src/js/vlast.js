7c76d1cc251c0cb994e43cdba8f46a2405c84e64
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2017-12-28 13:05:11 +0600

    Now supports vlast.kz, working on internal headlines

--- src/js/vlast.js
+++ src/js/vlast.js
@@ -1,4 +1,9 @@
-const news = document.querySelectorAll('.article-caption .title > a, .anatylic-item-title > b, .informer-news-item-in > a, h1.title');
+const news = document.querySelectorAll('.article-caption .title > a,' +
+    ' .anatylic-item-title > b, ' +
+    '.informer-news-item-in > a, ' +
+    'h1.title, ' +
+    '.news-item-expand-title > h1 > a,' +
+    '.news-inline-item-title > a > b');
 for (let i = 0; i < news.length; i++) {
     let headline = news[i];
     if ((headline.textContent.substr(-3) !== '...') && (headline.textContent.substr(0, 12) !== 'Мир за ночь.')) {
