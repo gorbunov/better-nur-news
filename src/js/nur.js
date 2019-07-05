6e162867c572448b4e1e81c8070bdabbb4c07249
Maxim Neverov <nevmn@mail.ru>
2019-07-05 21:21:08 +0600

    Fix nur.kz

--- src/js/nur.js
+++ src/js/nur.js
@@ -1,6 +1,11 @@
-    const news = document.querySelectorAll('.news__caption, .article__caption');
-    for (let i = 0; i < news.length; i++) {
-        let headline = news[i];
-        headline.textContent = 'Вопреки здравому смыслу, ' + headline.textContent;
-        //console.log(headline.textContent);
-    }
+const news = document.querySelectorAll('.article-headline, ' +
+    '.article-footer__post-link, ' +
+    '.post-preview-mixed__title, ' +
+    '.post-preview-mixed-horizontal__title, ' +
+    '.post-preview-text__title, ' +
+    '.post-preview-mixed-big__title');
+for (let i = 0; i < news.length; i++) {
+    let headline = news[i];
+    headline.textContent = 'Вопреки здравому смыслу, ' + headline.textContent;
+    //console.log(headline.textContent);
+}
